ae44d40b5deaf4e9dfaa796d6c11fbf4291387ec
meelan10 <milanwagle73@gmail.com>
2026-05-19 15:20:42 +0545

    two way binding

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [title, settitle] = useState('')
+  const [details, setdetails] = useState('')
+  const [task, settask] = useState([])
+  
   const submithandler = (e) =>{
     e.preventDefault()
-    console.log('form submitted')
 
+    const copyTask = [...task];
+    copyTask.push({title, details})
+    settask(copyTask)
+   
+
+    settitle('')
+    setdetails('')
   } 
+
   return (
+
     <div className='h-screen lg:flex bg-black text-white'>
       <form onSubmit={(e)=>{
         submithandler(e)
       }}
+
       className='lg:w-1/2 flex flex-col items-start gap-4 p-10 '>
            <h1 className='text-3xl font-bold'>Add Note</h1>
 
+
          <input 
-        className='px-5 w-full  py-2 border-2 outline-none rounded' type='text' 
+        className='px-5 w-full  py-2 border-2 outline-none rounded text-white' type='text' 
         placeholder='Enter Notes Heading' 
-        />
-        <textarea 
-        className='px-5 font-medium  w-full h-32 flex py-2 border-2 rounded outline-none items-start flex-row' 
-        placeholder='Enter deatils' 
-        type='text' >
-        </textarea>
+        value={title}
+        onChange={(e)=> {
+          settitle(e.target.value)
+        }}
         
-        <button className='bg-white font-medium w-full outline-none text-black px-5 py-2 rounded'>Add Note</button>
+        />
+
+
+        <textarea 
+        className='px-5 font-medium  w-full h-32 py-2 border-2 rounded outline-none' 
+        placeholder='Enter details' 
+        value={details}  
+        onChange={(e)=>{
+          setdetails(e.target.value)
+        }}
+       />
+        
+        
+
+        <button className='bg-white active:bg-gray-400  active:scale-95 font-medium w-full outline-none text-black px-5 py-2 rounded'
+        >Add Note
+        </button>
 
       </form>
 
-      <div className='lg:w-1/2 border-l-2 p-10'>
-      <h1 className='text-3xl font-bold'>Your NOtes</h1>
+      <div className='lg:w-1/2 bg-white text-black p-10'>
+      <h1 className='text-3xl font-bold'>Your Notes</h1>
         <div className='flex flex-wrap gap-5  mt-5 overflow-auto'> 
 
-            <div className='h-52 w-40 rounded-2xl bg-white'></div>
-            <div className='h-52 w-40 rounded-2xl bg-white'></div>
-              <div className='h-52 w-40 rounded-2xl bg-white'></div>
+            function
 
         </div>
       </div>
